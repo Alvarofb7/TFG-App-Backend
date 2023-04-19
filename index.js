09addffd602bfb412a3f9ace9f5109205939577a
Alvaro <alvfribal@alum.us.es>
2023-04-19 10:19:57 +0200

    #2 - Integrar base de datos

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const { dbConnection } = require("./database/config");
+const cors = require('cors');
 require("dotenv").config();
 
 // Crear el servidor de express
 const app = express();
 
+// Base de datos
+dbConnection();
+
+// CORS
+app.use(cors());
+
+// Directorio Público
+app.use(express.static("public"));
+
+
+app.use("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Escuchar el puerto
 app.listen(process.env.PORT, () => {
